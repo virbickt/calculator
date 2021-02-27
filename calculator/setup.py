9c31cf99745213b031a3ef84213440b7d734c4c0
virbickt <63246400+virbickt@users.noreply.github.com>
2021-02-27 15:41:03 +0200

    Delete calculator directory

--- calculator/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-  name="calculator",
-  version="0.0.1",
-  description="Calculator with five basic operations",
-  py_modules=["calculator"],
-  package_dir={"": "package"},
-)
