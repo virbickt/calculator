23ca1ad4da79524d9526f3c95f127f54708fccda
virbickt <63246400+virbickt@users.noreply.github.com>
2021-02-27 17:18:49 +0200

    Add files via upload

---
+++ calculator/calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def __init__(self, state: float = 0):
+        if not isinstance(state, (int, float)):
+            raise TypeError
+        self._state = state
+
+    @property
+    def state(self) -> float:
+        return self.__state
