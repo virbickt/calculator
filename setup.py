e3ff4b47818eaa3617fdabe70ca267f51190a8d8
virbickt <63246400+virbickt@users.noreply.github.com>
2021-02-27 17:00:25 +0200

    Create setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
   name="calculator",
   version="0.0.1",
   description="Calculator with five basic operations",
-  py_modules=["tesla"],
-  package_dir={"": "package"},
+  py_modules=["calculator"],
 )
