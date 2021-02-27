ce592a46081ad92e7c7e773f96b9b38b58b55f04
virbickt <63246400+virbickt@users.noreply.github.com>
2021-02-27 17:25:45 +0200

    Delete calculator.py

--- calculator/calculator.py
+++
@@ -1,9 +0,0 @@
-class Calculator:
-    def __init__(self, state: float = 0):
-        if not isinstance(state, (int, float)):
-            raise TypeError
-        self.__state = state
-
-    @property
-    def state(self) -> float:
-        return self.__state
