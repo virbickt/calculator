fc69fdfd9b537ca7d930c06a222379b407e8da06
virbickt <63246400+virbickt@users.noreply.github.com>
2021-03-03 15:43:29 +0200

    Update test_divide.py

--- tests/test_divide.py
+++ tests/test_divide.py
@@ -1,8 +1,13 @@
 import pytest
 
 
-def canDivide():
+def test_divide():
     calculator = Calculator()
     calculator.add(6)
     result = calculator.divide(3)
     assert result == 2
+    
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        calculator = Calculator()
+        calculator.divide(0)
